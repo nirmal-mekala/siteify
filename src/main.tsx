06736ef5981839fa50d09cfe84b4fb3c8d694fb4
Nirmal Mekala <ndmekala@icloud.com>
2025-05-23 08:43:00 -0500

    improve themes and hover styles etc

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app.tsx';
 
 const container = document.body.querySelector('#page');
 
+container.insertBefore(document.createElement('hr'), container.firstChild);
 container.insertBefore(document.createElement('header'), container.firstChild);
 
 render(<App />, container.firstChild);
