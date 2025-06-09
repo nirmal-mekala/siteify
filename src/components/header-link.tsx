5938296e9623b9f7c776798413f7315a9e9cc459
Nirmal Mekala <ndmekala@icloud.com>
2025-06-09 07:14:49 -0500

    progress + 1 theme done

--- src/components/header-link.tsx
+++ src/components/header-link.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 
 function HeaderLink() {
   const [isHovered, setIsHovered] = useState(false);
+  const [theme, setTheme] = useState('light');
+  // light funk dark fancy
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -10,6 +12,22 @@ function HeaderLink() {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+
+  const themeUpdater = (theme: string) => {
+    return () => setTheme(theme);
+  };
+
+  useEffect(() => {
+    document.body.classList.remove(
+      'light',
+      'dark',
+      'fancy',
+      'funk',
+      'rgb',
+      'pattern',
+    );
+    document.body.classList.add(theme);
+  }, [theme]);
 
   return (
     <div>
@@ -24,6 +42,15 @@ function HeaderLink() {
         {isHovered && <span>.</span>}
         <span>la</span>
       </a>
+      <div>
+        <span style={{ marginRight: '0.5rem' }}>selected: {theme}</span>
+        <button onClick={themeUpdater('light')}>1</button>
+        <button onClick={themeUpdater('dark')}>2</button>
+        <button onClick={themeUpdater('fancy')}>3</button>
+        <button onClick={themeUpdater('funk')}>4</button>
+        <button onClick={themeUpdater('rgb')}>5</button>
+        <button onClick={themeUpdater('pattern')}>6</button>
+      </div>
     </div>
   );
 }
