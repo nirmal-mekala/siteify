bb9836a4ed06d27f410b970c16b17ecc2cd608b6
Nirmal Mekala <ndmekala@icloud.com>
2025-06-30 15:25:55 -0500

    resolve hr color issue by just using border

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { App } from './app.tsx';
 
 const container = document.body.querySelector('#page');
 
-container.insertBefore(document.createElement('hr'), container.firstChild);
 container.insertBefore(document.createElement('header'), container.firstChild);
 
 render(<App />, container.firstChild);
