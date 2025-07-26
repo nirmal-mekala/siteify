04cbbe560b8e90caf09fab7da5c377a0cf1501a2
Nirmal Mekala <ndmekala@icloud.com>
2025-07-25 21:01:53 -0500

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { App } from './app.tsx';
 const container = document.body.querySelector('#page');
 if (container) {
   container.insertBefore(document.createElement('header'), container.firstChild);
-  if(container.firstChild) {
+  if (container.firstChild) {
     render(<App />, container.firstChild);
   }
 }
